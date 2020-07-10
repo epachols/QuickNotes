b189e2b6e3a369e5419001fcfe90670902637fbe
Capitol Heel <66080081+epachols@users.noreply.github.com>
2020-07-09 18:24:56 -0700

    beginning methods

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,26 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//make get handlers here
+//make post handlers here
+
+
+
+
+
+
+//setting up to listen:
+app.listen(PORT, function() {
+    console.log("App listening on PORT http://localhost:" + PORT);
+  });
